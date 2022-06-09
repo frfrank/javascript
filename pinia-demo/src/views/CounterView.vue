6d374a8de994a5c05480228420adcb702e2450a4
Francisco Picado <franciscopicado19@gmail.com>
2022-06-09 19:15:25 +0200

    🌱  example vue pinia

---
+++ pinia-demo/src/views/CounterView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        Counter view {{counterStore.counter}} {{name}}
+    </div>
+    <button @click="counterStore.incrementBy(1)">+1</button>
+    <button @click="counterStore.incrementBy(2)">+2</button>
+    <button @click="counterStore.incrementBy(3)">+3</button>
+
+</template>
+
+<script lang="ts" setup>
+import { userCounterStore } from '../stores/counter';
+
+ const counterStore = userCounterStore();
+
+const name = "Francisco picado";
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pinia-demo/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Home View:   {{counterStore.counter}}
+    </div>
+
+    <h3>Numero de cambios: {{ counterStore.numberOfChanges}}</h3>
+</template>
+
+<script lang="ts" setup>
+import { userCounterStore } from '../stores/counter';
+
+ const counterStore = userCounterStore();
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
